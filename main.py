a452a6a3898b607675bc98ecec069c8f84495f6d
Sultan <92447824+SulRash@users.noreply.github.com>
2022-10-21 16:46:13 +0100

    Dumping game files
    Just a quick dump of all the necessary code to run the game from the terminal window.

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+from src.game import Game
+
+def main():
+    
+    if int(input("Do you want to add a seed? (0 for no, 1 for yes)\n")):
+        seed = int(input("Please write the seed down as an integer. Best to make it more than 5 Digits.\n"))
+        fixed_seed = True
+    else:
+        seed = 0
+        fixed_seed = False
+    
+    game = Game(seed=seed, fixed_seed=fixed_seed)
+    game.start()   
+
+if __name__ == '__main__':
+    main()
---
+++ src/world/tile.py
@@ -0,0 +1,36 @@
+class Tile:
+
+    def __init__(self, blocks_movement: bool, see_through: bool, char: str, dark: str, explored: bool) -> None:
+        """
+        Tile class for walls and floors. Could be extendable to other types too.
+
+        Args:
+            blocks_movement (bool): If tile blocks movement for entities.
+            see_through (bool): If entities can see through the tile.
+            char (str): How it is rendered in default view
+            dark (str): [In-Progress] To be used to rendering a different view.
+            explored (bool): If tile is explored by player.
+        """
+        self.dark = dark
+        self.char = char
+        self.see_through = see_through
+        self.blocks_movement = blocks_movement
+        self.explored = explored
+
+floor = Tile(
+    blocks_movement=False,
+    see_through=True,
+    dark="▩",
+    char=".",
+    explored=False
+    #light="▣"
+)
+
+wall = Tile(
+    blocks_movement=True,
+    see_through=False,
+    dark="■",
+    char="#",
+    explored=False
+    #light="▥"
+)
